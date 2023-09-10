16c9c70730ec43aef76620937d8a2fe557e32dcf
Alan dev <Alan.jimenez353@gmail.com>
2023-09-10 19:40:09 -0300

    First Commit

---
+++ Examples/springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/AppConfig.java
@@ -0,0 +1,14 @@
+package com.formacionbdi.springboot.app.item;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+	@Bean("clienteRest")
+	public RestTemplate registrarRestTemplate() {
+		return new RestTemplate();
+	}
+}
---
+++ Examples/springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -0,0 +1,29 @@
+package com.formacionbdi.springboot.app.productos.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formacionbdi.springboot.app.productos.models.entity.Producto;
+import com.formacionbdi.springboot.app.productos.models.service.IProductoService;
+
+@RestController
+public class ProductoController {
+	
+	@Autowired
+	private IProductoService productoService;
+	
+	@GetMapping("/listar")
+	public List<Producto> listar(){
+		return productoService.findAll();
+	}
+	
+	@GetMapping("/ver/{id}")
+	public Producto detalle(@PathVariable Long id) {
+		return productoService.findById(id);
+	}
+
+}
