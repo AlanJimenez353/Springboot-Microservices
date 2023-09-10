1a3fdd2028fce64fb64be3f5cc667a78829baf21
Alan dev <Alan.jimenez353@gmail.com>
2023-09-10 19:54:49 -0300

    Added Eureka Server

---
+++ Examples/springboot-service-gateway-server/src/main/java/com/springboot/app/gateway/SpringbootServiceGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiceGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceGatewayServerApplication.class, args);
+	}
+
+}
